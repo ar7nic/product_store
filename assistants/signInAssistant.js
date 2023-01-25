73cdbd6740224f44c7e22fc791a6dc4abb49004b
Ar7nic <ar7nic@gmail.com>
2023-01-25 14:07:03 +0200

    fix

--- assistants/signInAssistant.js
+++ assistants/signInAssistant.js
@@ -4,9 +4,9 @@ export class SignInAssistant {
   async signInToSite(page, user) {
     await PAGES.mainMenu.signInMenu.clickElem(page);
     // await page.locator(PAGES.mainMenu.signInMenu).click();
-    await PAGES.signInPopUp.signInUserNameInput.fillField(page,user.name)
+    await PAGES.signInPopUp.signInUserNameInput.fillField(page, user.userName)
     // await page.locator(PAGES.signInPopUp.signInUserNameInput).fill(name);
-    await PAGES.signInPopUp.signInPasswordInput.fillField(page,user.password);
+    await PAGES.signInPopUp.signInPasswordInput.fillField(page,user.userPassword);
     // await page.locator(PAGES.signInPopUp.signInPasswordInput).fill(user.password);
     await PAGES.signInPopUp.signUpButton.clickElem(page);
     // await page.locator(PAGES.signInPopUp.signUpButton).click();
