a3aabbbe8b8a4286e3da09ba3f01831f57e69f40
Ar7nic <ar7nic@gmail.com>
2023-02-02 10:18:55 +0200

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -48,12 +48,12 @@ const config = {
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: {
-    //     ...devices['Desktop Chrome'],
-    //   },
-    // },
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
 
     {
       name: 'firefox',
@@ -91,12 +91,12 @@ const config = {
     //     channel: 'msedge',
     //   },
     // },
-    {
-      name: 'Google Chrome',
-      use: {
-        channel: 'chrome',
-      },
-    },
+    // {
+    //   name: 'Google Chrome',
+    //   use: {
+    //     channel: 'chrome',
+    //   },
+    // },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
